111c565b3e266ef9f8ed1a772b1d4aa48be7b192
skid901 <tlrudejr1993@naver.com>
2018-10-13 17:49:19 +0900

    20181013 1749 그래프 및 컬럼버튼 구현

--- HaneumProject/static/haneum/meta/analysis.js
+++ HaneumProject/static/haneum/meta/analysis.js
@@ -1,29 +1,58 @@
-var column_translation = {
+let column_translation = {
     'CompanyName': '회사명',
-    'Year': '년도',
-    'Sales': '매출액',
-    'OperatingProfit': '영업이익',
-    'NetIncome': '당기순이익',
-    'OperatingActivitiesCashFlow': '영업활동으로인한현금흐름',
+	'Year': '년도',
+	'Bankruptcy': '부도여부',
+
+	'Inventory': '재고자산',
+	'AccountsReceivable': '매출채권',
+	'InventoryTotalAssets': '자산총계',
+	'TotalBorrowings': '총차입금',
+	'TotalLiabilities': '부채총계',
+	'TotalCapital': '자본총계',
+
+	'Sales': '매출액',
+	'CostofGoodsSold': '매출원가',
+	'OperatingProfit': '영업이익',
+	'FinancialCosts': '금융비용(손익)',
+	'NetIncome': '당기순이익',
+
+	'OperatingActivitiesCashFlow': '영업활동으로인한현금흐름',
     'InvestmentActivitiesCashFlow': '투자활동으로인한현금흐름',
     'FinancialActivitiesCashFlow': '재무활동으로인한현금흐름',
-    'AccountsReceivable': '매출채권',
+    
     'AccountsReceivableTurnover': '매출채권회전율',
     'AccountsReceivableTurnoverDays': '매출채권회전일수',
-    'CostofGoodsSold': '매출원가',
-    'Inventory': '재고자산',
     'InventoryTurnover': '재고자산회전율',
     'InventoryTurnoverDays': '재고자산회전일수',
-    'InventoryTotalAssets': '자산총계',
-    'TotalBorrowings': '총차입금',
-    'FinancialCosts': '금융비용(손익)',
-    'TotalLiabilities': '부채총계',
-    'TotalCapital': '자본총계',
-    'DebtRatio': '부채비율',
-    'Bankruptcy': '부도여부'
+	'DebtRatio': '부채비율'    
 };
 
-var totalData;
+let column_color = {
+	'Inventory': 'Crimson',
+	'AccountsReceivable': 'Coral',
+	'InventoryTotalAssets': 'Coral',
+	'TotalBorrowings': 'DarkGreen',
+	'TotalLiabilities': 'DarkBlue',
+	'TotalCapital': 'DarkViolet',
+
+	'Sales': 'FireBrick',
+	'CostofGoodsSold': 'GoldenRod',
+	'OperatingProfit': 'MediumSeaGreen',
+	'FinancialCosts': 'RoyalBlue',
+	'NetIncome': 'Plum',
+
+	'OperatingActivitiesCashFlow': 'LightPink',
+    'InvestmentActivitiesCashFlow': 'LightGreen',
+    'FinancialActivitiesCashFlow': 'LightSalmon',
+};
+
+let totalData;
+
+let checked_col_btn_list = [];
+
+let color = ['darkred', 'steelblue', 'DarkGreen', 'OrangeRed',
+			'DarkOrchid', 'DeepPink', 'GoldenRod', 'Indigo',
+];
 
 $('#upload').ajaxForm({
 	url: "./upload",
@@ -32,18 +61,13 @@ $('#upload').ajaxForm({
 	success: function (result, status, xhr) {
 		if (result.status == 1) {
 			totalData = result.data;
-			var parseDate = d3.timeParse("%Y");
-
+			let parseDate = d3.timeParse("%Y");
 			totalData = totalData.map(function(d) {
 				d.Year = parseDate(d.Year);
 				return d;
 			});
-
 			$('#graph').empty();
-			getGraph(totalData, [
-				'TotalLiabilities', 'TotalCapital'
-			]);
-			
+			getGraph(totalData, checked_col_btn_list);
 			$('#articles')
 				.css('display', 'block');
 			$('#analysis')
@@ -54,7 +78,7 @@ $('#upload').ajaxForm({
 			$('#id_file').val("");
 		} else {
 			console.log("return status : %d", result.status);
-			alert('파일을 업로드해주세요');
+			alert('파일을 업로드해주세요.');
 		}
 	},
 	error: function (xhr, status, error) {
@@ -62,42 +86,42 @@ $('#upload').ajaxForm({
 	}
 });
 
-$('#test_btn1').on('click', function() {
+$('.col_btn').on('click', function(event) {
+	let col_btn = event.target
+	let input = $(col_btn).children()[0];
+	let isChecked = $(input).prop('checked');
+	if (!isChecked) {
+		let col_name = $(input).val();
+		checked_col_btn_list.push(col_name);
+		$(input).prop('checked', true);
+	} else {
+		let col_name = $(input).val();
+		checked_col_btn_list = checked_col_btn_list.filter(function(el){
+			return el !== col_name
+		});
+		$(input).prop('checked', false);
+	}
 	$('#graph').empty();
-	getGraph(totalData, [
-		'TotalLiabilities', 
-		'TotalCapital'
-	]);
+	getGraph(totalData, checked_col_btn_list);
+	if(checked_col_btn_list.length == 0) {
+		alert('선택된 계정이 없습니다.\n계정을 선택해주십시오.');
+	}
 });
 
-$('#test_btn2').on('click', function() {
-	$('#graph').empty();
-	getGraph(totalData, [
-		'OperatingActivitiesCashFlow',
-    	'InvestmentActivitiesCashFlow',
-		'FinancialActivitiesCashFlow'
-	]);
-});
-
-$('#test_btn3').on('click', function() {
-	$('#graph').empty();
-	getGraph(totalData, [
-		'Sales',
-    	'OperatingProfit',
-    	'NetIncome'
-	]);
-});
+let svg = d3.select('svg');
 
 function getGraph(row_data, col_list) {  //, CompanyName, [col1, col2, col3]) {
 
 	let data = row_data;
+	
+	let svg = d3.select('svg')
+		.attr('width', window.innerWidth * 960 / 1280);
 
 	let top = 20;
 	let right = 20;
 	let bottom = 20;
 	let left = 90;  // left: 40
 
-	let svg = d3.select('svg');
 	let margin = { 'top': top, 'right': right, 'bottom': bottom, 'left': left };
 	let width = + svg.attr('width') - left - right;
 	let height = + svg.attr('height') - top - bottom;
@@ -130,7 +154,10 @@ function getGraph(row_data, col_list) {  //, CompanyName, [col1, col2, col3]) {
 		.domain([yMin, yMax])
 		.range([height, 0]);
 
-	let xAxis = d3.axisBottom(x);
+	let xAxis = d3
+		.axisBottom(x)
+		.tickFormat(d3.timeFormat('%Y'))
+		.ticks(d3.timeYear);
 	let yAxis = d3.axisLeft(y);
 
 	chart
@@ -142,31 +169,43 @@ function getGraph(row_data, col_list) {  //, CompanyName, [col1, col2, col3]) {
 		.append('g')
 		.call(yAxis);
 
+	
 	let div = d3
 		.select('body')
 		.append('div')
 		.attr('class', 'tooltip')
 		.style('opacity', 0)
 		.style('display', 'none')
-		.style('width', '170px')
-		.style('height', '35px');
+		.style('width', '160px')
+		.style('height', '65px');
 
 	let formatTime = d3.timeFormat('%Y');
+
+	if (yMin < 0) {
+		let zero_line = d3.line()
+			.x(d => x(d.Year))
+			.y(d => y(0));
+
+		chart
+			.append('path')
+			.datum(data)  // [data]
+			.attr('fill', 'none')
+			.attr('stroke', 'black')
+			.attr('stroke-width', 1)
+			.attr('stroke-dasharray', '5,5')
+			.attr('d', zero_line);
+	}
 
 	col_list.map(function(col_name, idx) {
 		let col_line = d3.line()
 			.x(d => x(d.Year))
 			.y(d => y(d[col_name]));
 
-		let color = ['darkred', 'steelblue', 'DarkGreen', 'OrangeRed',
-			'DarkOrchid', 'DeepPink', 'GoldenRod', 'Indigo',
-		];
-
 		chart
 			.append('path')
 			.datum(data)  // [data]
 			.attr('fill', 'none')
-			.attr('stroke', color[idx])
+			.attr('stroke', column_color[col_name])
 			.attr('stroke-width', 2)
 			.attr('d', col_line);
 
@@ -178,8 +217,8 @@ function getGraph(row_data, col_list) {  //, CompanyName, [col1, col2, col3]) {
 			.attr('r', 3)
 			.attr('cx', d => x(d.Year))
 			.attr('cy', d => y(d[col_name]))
-			.attr('class', col_name + 'Dots')
-			.style('fill', color[idx]);
+			.attr('class', `${col_name}Dots`)
+			.style('fill', column_color[col_name]);
 		
 		chart
 			.selectAll(`.${col_name}Dots`)
@@ -193,12 +232,16 @@ function getGraph(row_data, col_list) {  //, CompanyName, [col1, col2, col3]) {
 				div
 					.html(`<table>
 								<tr>
-									<td style="text-align: right;">연&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp도&nbsp;:</td>
-									<td style="text-align: left;">&nbsp;${formatTime(d.Year)}</td>
+									<td style="text-align: center;">연&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp도&nbsp;:</td>
 								</tr>
 								<tr>
-									<td style="text-align: right;">${column_translation[col_name]}&nbsp;:</td>
-									<td>&nbsp;￦&nbsp;${Number(d[col_name]).toLocaleString('en')}</td>
+									<td style="text-align: center;">${formatTime(d.Year)}</td>
+								</tr>
+								<tr>
+									<td style="text-align: center;">${column_translation[col_name]}&nbsp;:</td>
+								</tr>
+								<tr>
+									<td style="text-align: center;">￦&nbsp;${Number(d[col_name]).toLocaleString('en')}</td>
 								</tr>
 							</table>`)
 					.style('left', (d3.event.pageX + 5) + 'px')
